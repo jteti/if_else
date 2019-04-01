f91806d36a5f0e4eea561893104468aae1ac00b9
jteti <mr.jteti@gmail.com>
2019-04-01 12:00:17 -0400

    Else statement added

--- script.js
+++ script.js
@@ -2,5 +2,7 @@ var firstName = 'John';
 var civilStatus = 'married';
 
 if (civilStatus === 'married') {
-  console.log(firstName + ' is married');  
+  console.log(firstName + ' is married');
+} else {
+  console.log(firstName + ' will hopefully marry soon :)';
 }
